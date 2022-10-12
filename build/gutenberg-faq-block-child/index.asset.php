0239996ce6db3baaac2ebde2c981ff7f49d8c387
hbhalodia <hit.bhalodia@rtcamp.com>
2022-10-12 17:26:12 +0530

    Add css for block

--- build/gutenberg-faq-block-child/index.asset.php
+++ build/gutenberg-faq-block-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b03d115a2bed9d4f3a9a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '6078a0fa8651a6256ec8');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd4c0321fb041e2980e73');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'aeb615fe85ce736ea466');
