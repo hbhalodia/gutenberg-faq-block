004748a2116035580af34135c2696168d854b5c6
hbhalodia <hit.bhalodia@rtcamp.com>
2022-10-12 14:57:13 +0530

    Add innerblocks parent block for the same

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f722da5183de5aa5e418');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '04529a3f757ea1236b29');
